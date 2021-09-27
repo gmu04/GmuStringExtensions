beaed56b99dafd903502ea2feb962b998da14ce1
GokhanMutlu <gokhanmutlu04@yahoo.com>
2021-09-27 08:33:19 +0300

    removing empty spaces

--- StringExtensions/String + base64.swift
+++ StringExtensions/String + base64.swift
@@ -14,14 +14,14 @@ public protocol Base64Codable{
 
 extension String:Base64Codable {
 	
-	public var base64Encoded: String? {
+	public var base64Encoded:String?{
 		let utf8 = self.data(using: .utf8)
 		let base64 = utf8?.base64EncodedString()
 		return base64
 	}
 	
-	public var base64Decoded: String? {
-		guard let base64 = Data(base64Encoded: self) else { return nil }
+	public var base64Decoded:String?{
+		guard let base64 = Data(base64Encoded: self) else{ return nil }
 		let utf8 = String(data: base64, encoding: .utf8)
 		return utf8
 	}
