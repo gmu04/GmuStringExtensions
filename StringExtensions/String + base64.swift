67dcefdb2c5cdd47e053fa4249e795073e61b5a5
GokhanMutlu <gokhanmutlu04@yahoo.com>
2021-09-27 08:32:03 +0300

    protocol Base64Codable added

---
+++ StringExtensions/String + base64.swift
@@ -0,0 +1,29 @@
+//
+//  String + encodable.swift
+//  StringExtensions
+//
+//  Created by Gokhan Mutlu on 27.09.2021.
+//
+
+import Foundation
+
+public protocol Base64Codable{
+	var base64Encoded:String?{ get }
+	var base64Decoded:String?{ get }
+}
+
+extension String:Base64Codable {
+	
+	public var base64Encoded: String? {
+		let utf8 = self.data(using: .utf8)
+		let base64 = utf8?.base64EncodedString()
+		return base64
+	}
+	
+	public var base64Decoded: String? {
+		guard let base64 = Data(base64Encoded: self) else { return nil }
+		let utf8 = String(data: base64, encoding: .utf8)
+		return utf8
+	}
+	
+}
